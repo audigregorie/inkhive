3fcb2c4cd6f762b1bc0980ac0ed4584eb74351ae
Audi Young <122812915+audigregorie@users.noreply.github.com>
2024-12-31 11:36:11 +0000

    feat: ui search

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,7 +1,12 @@
+import { IoSearch } from 'react-icons/io5';
+
 const Search = () => {
   return (
-    <div>
-      <div>Search</div>
+    <div className="flex items-center gap-2 rounded-full bg-gray-100 p-2">
+      <span className="text-gray-400">
+        <IoSearch />
+      </span>
+      <input type="text" placeholder="search a post..." className="bg-transparent outline-none" />
     </div>
   );
 };
